6f80fcc0b2b63460a2a21b99cd842ab1f91901c8
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-08-08 00:52:56 +0700

    edit status

--- src/pages/fram-detail/fram-detail.ts
+++ src/pages/fram-detail/fram-detail.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
-import  {UploadsSharedUploadProvider} from '../../providers/uploads-shared-upload/uploads-shared-upload';
+import { UploadsSharedUploadProvider } from '../../providers/uploads-shared-upload/uploads-shared-upload';
 import { Upload } from '../../providers/uploads-shared-upload/upload';
 /**
  * Generated class for the FramDetailPage page.
@@ -16,26 +16,34 @@ import { Upload } from '../../providers/uploads-shared-upload/upload';
   templateUrl: 'fram-detail.html',
 })
 export class FramDetailPage {
-	image:string;
-	moisture:number;
-	status:boolean;
-	key:string;
-	items: any;
-  datafire:any;
+  image: string;
+  moisture1: number;
+  moisture2: number;
+  moisture3: number;
+  status: boolean;
+  key: string;
+  items: any;
+  datafire: any;
   selectedFiles: FileList;
   currentUpload: any;
+  zone: NgZone;
 
-
-  constructor(private upSvc:UploadsSharedUploadProvider,private af: AngularFireDatabase,public navCtrl: NavController, public navParams: NavParams ) {
-  	this.image = navParams.get('image');
-  	this.moisture = navParams.get('moisture');
-  	this.status = navParams.get('status');
-  	this.key = navParams.get('key');
-  	this.af.object(this.key).subscribe((data)=>{
-    this.datafire=this.af.list('https://app-project-d97a3.firebaseio.com/');
-      this.items={
-        image:data.image
-      }
+  constructor(private upSvc: UploadsSharedUploadProvider, private af: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
+    this.zone = new NgZone({});
+    this.image = navParams.get('image');
+    this.status = navParams.get('status');
+    this.key = navParams.get('key');
+    this.af.object(this.key).subscribe((data) => {
+      this.zone.run(() => {
+        this.datafire = this.af.list('https://app-project-d97a3.firebaseio.com/');
+        this.moisture1 = data.moisture1;
+        this.moisture2 = data.moisture2;
+        this.moisture3 = data.moisture3;
+        this.status = data.status;
+        this.items = {
+          image: data.image
+        }
+      });
       console.log(this.items)
     });
   }
@@ -45,18 +53,18 @@ export class FramDetailPage {
     console.log('ionViewDidLoad FramDetailPage');
   }
   notify() {
-  this.datafire.update(this.key, {status:this.status}); 
-  console.log("Toggled: "+ this.status); 
-}
+    this.datafire.update(this.key, { status: this.status });
+    console.log("Toggled: " + this.status);
+  }
 
-detectFiles(event) {
-      this.selectedFiles = event.target.files;
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
   }
 
   uploadSingle() {
     let file = this.selectedFiles.item(0)
     this.currentUpload = new Upload(file);
-    this.upSvc.pushUpload(this.currentUpload,this.key)
+    this.upSvc.pushUpload(this.currentUpload, this.key)
   }
 
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,57 +9,58 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class HomePage {
 
-items: FirebaseListObservable<any[]>;   
-   key:string;   
-   topic:string;   
-   dueDate:string;   
-   isToogle:boolean = false; 
-   
+  items: FirebaseListObservable<any[]>;
+  key: string;
+  topic: string;
+  dueDate: string;
+  isToogle: boolean = false;
+
   constructor(
-  				private af: AngularFireDatabase,
-                 public navCtrl: NavController,                
-                 public navParams: NavParams) {
-      
-  	}
+    private af: AngularFireDatabase,
+    public navCtrl: NavController,
+    public navParams: NavParams) {
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FirebasePage');
   }
-   ionViewWillEnter() {     
-   	this.showData();   
-   }
+  ionViewWillEnter() {
+    this.showData();
+  }
 
-   showData() {     
-   	this.items=this.af.list('https://app-project-d97a3.firebaseio.com/');//.subscribe(item => console.log(item)); //ใส url ของผูอาน   
-   }
+  showData() {
+    this.items = this.af.list('https://app-project-d97a3.firebaseio.com/');//.subscribe(item => console.log(item)); //ใส url ของผูอาน   
+  }
 
-   select(item) {     //console.log(item);     
-   	this.isToogle = true;     
-   	this.topic = item.note.topic;     
-   	this.dueDate = item.note.dueDate;     
-   	this.key = item.$key;   
-   	} 
-   	 save(note: any) {     //console.log(blog);     
-   	 	this.items.push({ note });     
-   	 	this.isToogle = false;   
-   	 } 
- 
-//อัปเดตขอมูลตาม key ที่สงมา   
-update(note: any) {     
-	if (this.key) {     
-		this.items.update(this.key, {note});     
-		this.isToogle = false;     
-	}   
-}
-delete(item:any) {     
-	this.items.remove(item.$key);     
-	this.isToogle = false;   
-}
-openProduct(prod){
-    this.navCtrl.push(FramDetailPage,{image:prod.image,
-      moisture:prod.moisture,
-      status:prod.status,
-      key:prod.$key
+  select(item) {     //console.log(item);     
+    this.isToogle = true;
+    this.topic = item.note.topic;
+    this.dueDate = item.note.dueDate;
+    this.key = item.$key;
+  }
+  save(note: any) {     //console.log(blog);     
+    this.items.push({ note });
+    this.isToogle = false;
+  }
+
+  //อัปเดตขอมูลตาม key ที่สงมา   
+  update(note: any) {
+    if (this.key) {
+      this.items.update(this.key, { note });
+      this.isToogle = false;
+    }
+  }
+  delete(item: any) {
+    this.items.remove(item.$key);
+    this.isToogle = false;
+  }
+  openProduct(prod) {
+    this.navCtrl.push(FramDetailPage, {
+      image: prod.image,
+      moisture: prod.moisture,
+      status: prod.status,
+      key: prod.$key
     });
-}
+  }
 }
